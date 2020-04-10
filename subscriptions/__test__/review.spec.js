9aa107469c56db73197b7821c553ef27d7f2d11f
dgaudet <dean@deangaudet.com>
2020-04-10 13:52:40 -0600

    Adding some more event tests to review.spec

--- subscriptions/__test__/review.spec.js
+++ subscriptions/__test__/review.spec.js
@@ -1,22 +1,31 @@
 const assert = require('assert')
 const ReviewProcess = require('../processes/Review')
 const MembershipApplication = require('../MembershipApplication')
+const sinon = require('sinon')
 
 describe('The Review Process', () => {
   describe('Receiving a valid application', () => {
     let decision
+    let validApp = new MembershipApplication({
+      first: 'Test',
+      last: 'User',
+      email: 'test@test.com',
+      age: 30,
+      height: 66,
+      weight: 180
+    })
+
+    let review = new ReviewProcess()
+    const validationSpy = sinon.spy()
+    const missionSpy = sinon.spy()
+    const roleAvailableSpy = sinon.spy()
+    const roleCompatibleSpy = sinon.spy()
 
     beforeEach(() => {
-      let validApp = new MembershipApplication({
-        first: 'Test',
-        last: 'User',
-        email: 'test@test.com',
-        age: 30,
-        height: 66,
-        weight: 180
-      })
-
-      let review = new ReviewProcess()
+      review.on('validated', validationSpy)
+      review.on('mission-selected', missionSpy)
+      review.on('role-available', roleAvailableSpy)
+      review.on('role-compatible', roleCompatibleSpy)
       review.processApplication(validApp, (err, result) => {
         decision = result
       })
@@ -24,6 +33,18 @@ describe('The Review Process', () => {
 
     it('returns success', () => {
       assert(decision.success, decision.message)
+    })
+    it('ensures the application is valid', () => {
+      assert(validationSpy.called)
+    })
+    it('selects a mission', () => {
+      assert(missionSpy.called)
+    })
+    it('ensure a role exists', () => {
+      assert(roleAvailableSpy.called)
+    })
+    it('ensures the role is compatible', () => {
+      assert(roleCompatibleSpy.called)
     })
   })
 })
