55032cabb41b95662ccf3e8d7cb87555e7b00138
dgaudet <dean@deangaudet.com>
2020-04-10 10:52:00 -0600

    Adding invalid test cases

--- subscriptions/__test__/mission_application.spec.js
+++ subscriptions/__test__/mission_application.spec.js
@@ -27,20 +27,64 @@ describe('Membership Application requirements', () => {
     it('all validators successful', () => {
       assert(validApp.isValid(), 'Not valid')
     })
-    it('email is 4 or more chars and contains an @', () => {
-      assert(validApp.emailIsValid())
+  })
+
+  describe('Application invalid if...', () => {
+    it('email is 4 characters or less', () => {
+      const invalidMembership = new MembershipApplication({email: 'd@d'})
+      assert(!invalidMembership.emailIsValid())
     })
-    it('height is between 60 and 75 inches', () => {
-      assert(validApp.heightIsValid())
+    it('email does not contain an @', () => {
+      const invalidMembership = new MembershipApplication({email: 'dd'})
+      assert(!invalidMembership.emailIsValid())
     })
-    it('age is between 15 and 100', () => {
-      assert(validApp.ageIsValid())
+    it('email is omitted', () => {
+      const invalidMembership = new MembershipApplication({})
+      assert(!invalidMembership.emailIsValid())
     })
-    it('weight is between 100 and 300', () => {
-      assert(validApp.weightIsValid())
+    it('height is less than 60', () => {
+      const invalidMembership = new MembershipApplication({height: 55})
+      assert(!invalidMembership.heightIsValid())
     })
-    it('first and last name are provided', () => {
-      assert(validApp.nameIsValid())
+    it('height is more than 75', () => {
+      const invalidMembership = new MembershipApplication({height: 80})
+      assert(!invalidMembership.heightIsValid())
+    })
+    it('height is omitted', () => {
+      const invalidMembership = new MembershipApplication({})
+      assert(!invalidMembership.heightIsValid())
+    })
+    it('age is more than 100', () => {
+      const invalidMembership = new MembershipApplication({age: 101})
+      assert(!invalidMembership.ageIsValid())
+    })
+    it('age is less than 15', () => {
+      const invalidMembership = new MembershipApplication({age: 14})
+      assert(!invalidMembership.ageIsValid())
+    })
+    it('age is omitted', () => {
+      const invalidMembership = new MembershipApplication({})
+      assert(!invalidMembership.ageIsValid())
+    })
+    it('weight is less than 100', () => {
+      const invalidMembership = new MembershipApplication({weight: 99})
+      assert(!invalidMembership.weightIsValid())
+    })
+    it('weight is more than 300', () => {
+      const invalidMembership = new MembershipApplication({weight: 300})
+      assert(!invalidMembership.weightIsValid())
+    })
+    it('weight is omitted', () => {
+      const invalidMembership = new MembershipApplication({})
+      assert(!invalidMembership.weightIsValid())
+    })
+    it('first name is omitted', () => {
+      const invalidMembership = new MembershipApplication({last: 'test'})
+      assert(!invalidMembership.nameIsValid())
+    })
+    it('last name is omitted', () => {
+      const invalidMembership = new MembershipApplication({first: 'hey'})
+      assert(!invalidMembership.nameIsValid())
     })
   })
 })
